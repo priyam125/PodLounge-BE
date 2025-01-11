3840e3958103aac5dcc2321e4a233af967501149
priyam125 <sinha.priyam002@gmail.com>
2025-01-12 01:36:45 +0530

    deployment fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const server = http.createServer(app); // Create a server
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://podlounge.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
@@ -38,7 +38,7 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://podlounge.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
